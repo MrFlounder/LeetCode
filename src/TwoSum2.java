5810b3606bf4fe9c296d19ca0c99f1203f7477a7
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-25 10:22:41 -0400

    better solution

--- src/TwoSum2.java
+++ src/TwoSum2.java
@@ -1,11 +1,30 @@
-import java.util.Hashtable;
+import java.util.*;
 
 /**
  * Created by guangshuozang on 8/24/15.
  */
 public class TwoSum2 {
-    Hashtable table = new Hashtable();
-    public void method(){
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> table = new HashMap<Integer, Integer>();
+        int[] temp = {0,0};
+        for(int i = 0; i < nums.length; i++) {
+                if (table.containsKey(target - nums[i])) {
+                    temp[1] = i + 1;
+                    temp[0] = table.get(target - nums[i]);
+                    return temp;
+                }
 
+                table.put(nums[i], i + 1);
+        }
+    return temp;
+}
+    public static void main(String arg[]){
+        Scanner in = new Scanner(System.in);
+        int[] numbers = {0,1,2,0};
+        TwoSum2 instance = new TwoSum2();
+        while(true) {
+            int target = in.nextInt();
+            System.out.println(Arrays.toString(instance.twoSum(numbers, target)));
+        }
     }
 }
