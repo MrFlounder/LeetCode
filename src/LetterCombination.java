a8c1c6082c893bfe5366a1a948c566aa035c4e59
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-29 18:53:11 -0400

    LetterCombination

---
+++ src/LetterCombination.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Created by guangshuozang on 8/29/15.
+ */
+public class LetterCombination {
+    public List<String> letterCombinations(String digits) {
+        String[] charmap = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        // LinkedList<String> result = new LinkedList<String>();
+        // result.addLast(null);
+        Vector<String> result = new Vector();
+        if(digits.length()==0)
+            return result;
+        String x = "";
+        result.add(x);
+
+        for(int i=0;i<digits.length();i++){
+            Vector<String> temp = new Vector();
+            String s = charmap[digits.charAt(i)-'0'];
+            for(int j=0;j<s.length();j++)
+                for(int k=0;k<result.size();k++){
+                    temp.add(result.get(k) + s.charAt(j));
+                }
+            result = temp;
+        }
+        return result;
+
+    }
+}
--- src/ThressSumClosest_16.java
+++ src/ThressSumClosest_16.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 /**
  * Created by guangshuozang on 8/29/15.
  */
 public class ThressSumClosest_16 {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = nums[0]+nums[1]+nums[nums.length-1];
+        for(int i=0;i<nums.length-2;i++){
+            int l=i+1,h=nums.length-1;
+            while(l<h){
+                if(nums[i]+nums[l]+nums[h]-target<0){
+                    closest=Math.abs(closest-target)<Math.abs(nums[i]+nums[l]+nums[h]-target)?closest:nums[i]+nums[l]+nums[h];
+                    l++;
+                }
+                else if(nums[i]+nums[l]+nums[h]-target>0){
+                    closest=Math.abs(closest-target)<Math.abs(nums[i]+nums[l]+nums[h]-target)?closest:nums[i]+nums[l]+nums[h];
+                    h--;
+                }
+                else
+                    return (nums[i]+nums[l]+nums[h]);
+            }
+        }
+        return closest;
+    }
 }
