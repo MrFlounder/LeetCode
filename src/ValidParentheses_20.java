3f0940310c95b1b6b6242149934267b881bffec1
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-31 19:08:56 -0400

    ValidParentheses

---
+++ src/ValidParentheses_20.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+/**
+ * Created by guangshuozang on 8/31/15.
+ */
+public class ValidParentheses_20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        // Iterate through string until empty
+        for(int i = 0; i<s.length(); i++) {
+            // Push any open parentheses onto stack
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+                stack.push(s.charAt(i));
+                // Check stack for corresponding closing parentheses, false if not valid
+            else if(s.charAt(i) == ')' && !stack.empty() && stack.peek() == '(')
+                stack.pop();
+            else if(s.charAt(i) == ']' && !stack.empty() && stack.peek() == '[')
+                stack.pop();
+            else if(s.charAt(i) == '}' && !stack.empty() && stack.peek() == '{')
+                stack.pop();
+            else
+                return false;
+        }
+        // return true if no open parentheses left in stack
+        return stack.empty();
+    }
+    public static void main(String arg[]){
+        String s = "()";
+        ValidParentheses_20 instance = new ValidParentheses_20();
+        instance.isValid(s);
+    }
+}
