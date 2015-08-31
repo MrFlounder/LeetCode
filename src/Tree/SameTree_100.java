b510a9320cd8b173a9d1623f5216910ec43a3307
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-30 20:02:43 -0400

    same tree

---
+++ src/Tree/SameTree_100.java
@@ -0,0 +1,19 @@
+package tree;
+
+import util.TreeNode;
+
+/**
+ * Created by guangshuozang on 8/30/15.
+ */
+public class SameTree_100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null)
+            return true;
+        else if(p==null||q==null)
+            return false;
+        else if(p.val!=q.val)
+            return false;
+        else
+            return isSameTree(p.right,q.right)&&isSameTree(p.left,q.left);
+    }
+}
---
+++ src/util/TreeNode.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Created by guangshuozang on 8/30/15.
+ */
+public class TreeNode {
+         public int val;
+         public TreeNode left;
+         public TreeNode right;
+         TreeNode(int x) { val = x; }
+     }
