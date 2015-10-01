e1d4a5d25136bb7519df6461e8a9717ead4aa20e
Guangshuo Zang <k.zanggs@gmail.com>
2015-09-30 23:14:34 -0400

    combinationSum2

---
+++ src/BackTracking/CombinationSum2_40.java
@@ -0,0 +1,34 @@
+package BackTracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by guangshuozang on 9/30/15.
+ */
+public class CombinationSum2_40 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(res,candidates,target,0,0,new ArrayList<Integer>());
+        return res;
+    }
+    public void helper(List<List<Integer>> res, int[] c, int t, int p, int sum, List<Integer> list){
+        if(sum==t){
+
+            res.add(new ArrayList<Integer>(list));
+            return;
+        }
+        if(sum>t) return;
+        while(p<c.length){
+            list.add(c[p]);
+            helper(res,c,t,p+1,sum+c[p],list);
+            list.remove(list.size()-1);
+            p++;
+            while(p<c.length&&c[p]==c[p-1]){
+                p++;
+            }
+        }
+    }
+}
