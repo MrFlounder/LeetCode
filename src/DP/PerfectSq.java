7c482353d0e23c83f0ff7c1564c8aa9d7cd4a64d
Guangshuo Zang <k.zanggs@gmail.com>
2015-10-10 23:21:31 -0400

    Perfect Square

---
+++ src/DP/PerfectSq.java
@@ -0,0 +1,20 @@
+package DP;
+
+/**
+ * Created by guangshuozang on 10/10/15.
+ */
+public class PerfectSq {
+    public int NumSquares(int n) {
+        int[] DP = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            int min= int.MaxValue;
+            for (int j = 1; j * j <= i; j++)
+            {
+                min= Math.min(min, DP[i - j * j] + 1);
+            }
+            DP[i] = min;
+        }
+        return DP[n];
+    }
+}
