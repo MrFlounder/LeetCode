a5e445444c872494297c1eaa43270a24da5d0020
Guangshuo Zang <k.zanggs@gmail.com>
2015-10-10 23:22:59 -0400

    update

--- src/DP/PerfectSq.java
+++ src/DP/PerfectSq.java
@@ -8,7 +8,7 @@ public class PerfectSq {
         int[] DP = new int[n + 1];
         for (int i = 1; i <= n; i++)
         {
-            int min= int.MaxValue;
+            int min= Integer.MAX_VALUE;
             for (int j = 1; j * j <= i; j++)
             {
                 min= Math.min(min, DP[i - j * j] + 1);
