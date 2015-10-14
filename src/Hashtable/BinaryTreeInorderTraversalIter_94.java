252e2a2281af1d2e1c2d03de02c59c89af69158c
Guangshuo Zang <k.zanggs@gmail.com>
2015-10-14 15:44:19 -0400

    Inorder traversal

---
+++ src/Hashtable/BinaryTreeInorderTraversalIter_94.java
@@ -0,0 +1,31 @@
+package Hashtable;
+
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by guangshuozang on 10/14/15.
+ */
+public class BinaryTreeInorderTraversalIter_94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+
+        while(cur!=null || !stack.empty()){
+            while(cur!=null){
+                stack.add(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+
+        return list;
+    }
+}
