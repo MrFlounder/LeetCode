01b94d97df6cb0dc71af0446eee7fd6eea47e4c1
Guangshuo Zang <k.zanggs@gmail.com>
2015-10-17 23:12:27 -0400

    flipgame2

--- src/BackTracking/FlipGame2_294.java
+++ src/BackTracking/FlipGame2_294.java
@@ -5,23 +5,20 @@ package BackTracking;
  */
 public class FlipGame2_294 {
     public boolean canWin(String s) {
-        if(s.length()==0||s.length()==1) return false;
-        return DFS(s, true);
-    }
-    public boolean DFS(String s, boolean isMyturn){
-        boolean r = true;
-        for(int i=1;i<s.length();i++){
-            if(s.charAt(i)==s.charAt(i-1)&&s.charAt(i)=='+'){
-                r=false;
-                break;
+        if (s == null || s.length() < 2) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.startsWith("++", i)) {
+                String t = s.substring(0, i) + "--" + s.substring(i + 2);
+
+                if (!canWin(t)) {
+                    return true;
+                }
             }
         }
-        if(r&&!isMyturn) return true;
-        for(int i=0;i<s.length()-1;i++){
-            if(s.charAt(i)!=s.charAt(i+1)||s.charAt(i)=='-') continue;
-            String t = s.substring(0, i) + "--" + s.substring(i + 2);
-            if(DFS(t,!isMyturn)) return true;
-        }
+
         return false;
     }
     public static void main(String arg[]){
