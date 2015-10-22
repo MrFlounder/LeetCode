3d600ac073dada364086805ca460eb496199257a
Guangshuo Zang <k.zanggs@gmail.com>
2015-10-21 21:23:12 -0400

    shortest  word dis

---
+++ src/Array/ShortestWordDis_243.java
@@ -0,0 +1,23 @@
+package Array;
+
+/**
+ * Created by guangshuozang on 10/21/15.
+ */
+public class ShortestWordDis_243 {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int p1 = -1, p2 = -1, min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1))
+                p1 = i;
+
+            if (words[i].equals(word2))
+                p2 = i;
+
+            if (p1 != -1 && p2 != -1)
+                min = Math.min(min, Math.abs(p1 - p2));
+        }
+
+        return min;
+    }
+}
