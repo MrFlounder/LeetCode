cefec11156b84b082e00b31efb553c4a884552a3
Guangshuo Zang <k.zanggs@gmail.com>
2015-10-22 16:53:05 -0400

    lowest common ancestor

---
+++ src/Tree/LowestCommonAncestorBST_236.java
@@ -0,0 +1,18 @@
+package tree;
+
+import util.TreeNode;
+
+/**
+ * Created by guangshuozang on 10/22/15.
+ */
+public class LowestCommonAncestorBST_236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }else{
+            return root;
+        }
+    }
+}
