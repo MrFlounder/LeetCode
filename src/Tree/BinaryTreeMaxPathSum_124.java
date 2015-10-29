a7c63b6a96ccdad33d188a74814a03b1a6a954f7
Guangshuo Zang <k.zanggs@gmail.com>
2015-10-28 20:38:53 -0400

    binary tree max path sum

---
+++ src/Tree/BinaryTreeMaxPathSum_124.java
@@ -0,0 +1,24 @@
+package tree;
+
+import util.TreeNode;
+
+/**
+ * Created by guangshuozang on 10/28/15.
+ */
+public class BinaryTreeMaxPathSum_124 {
+    int maxValue;
+
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        maxPathDown(root);
+        return maxValue;
+    }
+
+    private int maxPathDown(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+        maxValue = Math.max(maxValue, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+}
