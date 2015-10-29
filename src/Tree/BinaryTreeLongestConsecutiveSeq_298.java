9c706e000554b9ba4c829ec5bbf6b05e3ef17059
Guangshuo Zang <k.zanggs@gmail.com>
2015-10-28 20:15:11 -0400

    BinaryTreeLongestConsecutive

---
+++ src/Tree/BinaryTreeLongestConsecutiveSeq_298.java
@@ -0,0 +1,24 @@
+package tree;
+
+import util.TreeNode;
+
+/**
+ * Created by guangshuozang on 10/28/15.
+ */
+public class BinaryTreeLongestConsecutiveSeq_298 {
+    private int max = 0;
+    public int longestConsecutive(TreeNode root) {
+        if(root == null) return 0;
+        helper(root, 0, root.val);
+        return max;
+    }
+
+    public void helper(TreeNode root, int cur, int target){
+        if(root == null) return;
+        if(root.val == target) cur++;
+        else cur = 1;
+        max = Math.max(cur, max);
+        helper(root.left, cur, root.val + 1);
+        helper(root.right, cur, root.val + 1);
+    }
+}
